307a4133392cf8130ad0e7a309a04ace59c64797
Simon <stombosa@mum.edu>
2020-04-19 21:09:42 -0500

    Updates

---
+++ precache-manifest.d73bb0c6a1053b91ac9e8ac55abf86fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51d62ce5470ca2549df6265df9efb27",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b68d4ac2b17471d9aad7",
+    "url": "/monsters-rolodex/static/css/main.9001d852.chunk.css"
+  },
+  {
+    "revision": "77d19fc2296beccfe3af",
+    "url": "/monsters-rolodex/static/js/2.5ad77bcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.5ad77bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68d4ac2b17471d9aad7",
+    "url": "/monsters-rolodex/static/js/main.30b294cd.chunk.js"
+  },
+  {
+    "revision": "779c1edafb9d6ee57b27",
+    "url": "/monsters-rolodex/static/js/runtime-main.094e9a2b.js"
+  }
+]);
